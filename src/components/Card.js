9ddc84f58aea67af756c51161954e98f9f56ad11
Robbie Greiner <robbieg@gmail.com>
2018-01-28 14:22:32 -0700

    added weather description

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,6 +5,7 @@ const Card = ({locationData}) => {
     return(
       <div className='city-card'>
         <h2>{locationData.current_observation.display_location.city}, {locationData.current_observation.display_location.state_name}</h2>
+        <h3>{locationData.current_observation.weather}</h3>
         <img src={locationData.current_observation.icon_url}></img>
         <h3>{locationData.current_observation.temp_f}° F</h3>
       </div>
