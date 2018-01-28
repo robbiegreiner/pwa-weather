a02e90c8cbdd3aef4fc010776e9eb310ba1093ad
Robbie Greiner <robbieg@gmail.com>
2018-01-28 13:29:12 -0700

    fetching weather data working

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import '../styles/App.css';
 import Header from './Header.js';
+import apiKey from '../apikey';
 
 class App extends Component {
-  // fetch(`http://api.wunderground.com/api/${apiKey}/conditions/hourly/forecast10day/q/${city}.json`)
-  //   .then(response => response.json())
-  //   .then(result => console.log(result));
+  constructor(){
+    super();
+    this.state = {
+      weatherData : {},
+    }
+  }
+
+  componentDidMount(){
+    this.fetchWeatherData("Denver, Colorado")
+  }
+
+  fetchWeatherData(city){
+    fetch(`http://api.wunderground.com/api/${apiKey}/conditions/hourly/forecast10day/q/${city}.json`)
+      .then(response => response.json())
+      .then(result => console.log(result));
+  }
+
 
 
   render() {
