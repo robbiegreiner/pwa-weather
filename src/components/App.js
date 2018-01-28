4ef7641c85015cb469a8bbb110681eefacb77b01
Robbie Greiner <robbieg@gmail.com>
2018-01-28 13:55:24 -0700

    adding city and fetching weeather

--- src/components/App.js
+++ src/components/App.js
@@ -8,26 +8,26 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      weatherData : {},
-      city: 'Denver, Colorado',
+      weatherData : [],
       cityList: []
     }
 
     this.addCity = this.addCity.bind(this);
   }
 
-  componentDidMount(){
-    this.fetchWeatherData(this.state.city)
-  }
+  // componentDidMount(){
+  //   this.fetchWeatherData(this.state.cityList)
+  // }
 
   addCity(city){
     this.setState({ cityList: [...this.state.cityList, city]})
+    this.fetchWeatherData(city);
   }
 
   fetchWeatherData(city){
     fetch(`http://api.wunderground.com/api/${apiKey}/conditions/hourly/forecast10day/q/${city}.json`)
       .then(response => response.json())
-      .then(result => this.setState({ weatherData: result }));
+      .then(result => this.setState({ weatherData: [...this.state.weatherData, result ]}));
   }
 
   render() {
